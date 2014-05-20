ee75ef5c2a05f7c86db560cb268552954635ef2d
sjarvie <shane.jarvie@gmail.com>
2014-05-19 18:32:27 -0700

    fixes

--- job_openings/app.rb
+++ job_openings/app.rb
@@ -12,29 +12,35 @@ require 'equivalent-xml'
 require 'rdf/rdfa'
 require 'sparql'
 
-# We reuse two values several times over the course of this app: the base address of the RDF schema and
+# We reuse two values several times over the course of this flow: the base address of the RDF schema and
 # the list of attributes that define our final schema.  So we define them here.
 SCHEMA = RDF::Vocabulary.new "http://schema.org/"
 
 SCHEMA_ATTRIBUTES = ["address_locality", "hiring_organization", "title", "description", "name"]
 
-app = Zillabyte.app "zillabyte_indeed"
+flow = Zillabyte.new "zillabyte_indeed"
 
 # We start by outputting the URLs from which we wish to extract information.
 # This has been simplified considerably to only deal one page and one search term.
-input = app.source do
-  next_tuple do
-    emit :url => "http://www.indeed.com/jobs/?q=web+crawling"
+flow.spout do |node|
+  node.emits [["feed", ["url"]]]
+
+  node.next_batch do |controller|
+    controller.emit "feed", {
+      "url" => "http://www.indeed.com/jobs/?q=web+crawling"
+    }
   end
 end
 
 # Next, we process each URL by interpreting it as an RDF graph.
-stream = input.each do
-  name "each_job_posting"
-  execute do |tup|
-    # We wrap the entire app in a begin-rescue block, so that for unexpected exceptions we do not break our app
+flow.each do |node|
+  node.name "each_job_posting"
+  node.emits [["indeed_job_posting", SCHEMA_ATTRIBUTES]]
+
+  node.execute do |controller, tup|
+    # We wrap the entire flow in a begin-rescue block, so that for unexpected exceptions we do not break our flow
     # completely.  Note that one should *NOT* catch 'Exception', ever--this breaks signal handling and prevents
-    # clean shutdown of a app.
+    # clean shutdown of a flow.
     begin
       # Using the third party library, we first load the graph...
       url = tup["url"]
@@ -73,19 +79,19 @@ stream = input.each do
         controller.emit "indeed_job_posting", solution.bindings.select { |attribute, *| SCHEMA_ATTRIBUTES.include? attribute.to_s }
       end
     rescue => e
-      # Print to standard error--this is more semantically correct and will show up during execution of `zillabyte apps:test`.
-      log "Error:\n" + e.message + "\n" + e.backtrace.join("\n")
+      # Print to standard error--this is more semantically correct and will show up during execution of `zillabyte flows:test`.
+      STDERR.puts "Error:\n" + e.message + "\n" + e.backtrace.join("\n")
     end
   end
 end
 
 # The last step, as always, is to sink our newly extracted information to the database for later analysis.
-stream.sink do
-  name "indeed_job_posting"
+flow.sink do |node|
+  node.name "indeed_job_posting"
 
-  column "address_locality", :string
-  column "hiring_organization", :string
-  column "title", :string
-  column "description", :string
-  column "name", :string
+  node.column "address_locality", :string
+  node.column "hiring_organization", :string
+  node.column "title", :string
+  node.column "description", :string
+  node.column "name", :string
 end
