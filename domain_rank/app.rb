be7a4fdaf9e066da064db08888fec5ba86b4c398
qiongyu <xiaoyingxu@cmu.edu>
2014-09-06 02:37:33 -0700

    update app with new syntax, it was way outdated

--- domain_rank/app.rb
+++ domain_rank/app.rb
@@ -5,35 +5,28 @@ require 'nokogiri'
 # Create our 'app', the DSL to help us orchestrate our app
 app = Zillabyte.app("domain rank")
 
-
 # A 'source' is the beginning of a apps.  All data originates from the source.
-# Below, we will simply find which extrenal domains a given page may link to. 
-stream = app.source do |h|
+stream = app.source("web_pages")
 
-  h.matches "select * from web_pages"  # Let's consume the whole web. 
-  h.emits [:source_domain, :target_domain]  # The data this source emits.
-  
-  
-  # This is called on every web page
-  h.execute do |tuple, controller|
+# This is called on every web page
+stream = stream.each do |tuple|
     
-    base_url = tuple['url']
-    html = tuple['html']
-    doc = Nokogiri::HTML(html)
+  base_url = tuple['url']
+  html = tuple['html']
+  doc = Nokogiri::HTML(html)
     
-    doc.css('a').each do |link| 
+  doc.css('a').each do |link| 
       
-      # What domain does this item link to? 
-      target_uri = URI.join( base_url, link['href'])
-      target_domain = target_uri.host.downcase
+    # What domain does this item link to? 
+    target_uri = URI.join( base_url, link['href'])
+    target_domain = target_uri.host.downcase
       
-      # Emit this back to the app.  This is important because it will allow
-      # Zillabyte to parallelize the operation
-      controller.emit :source_domain => source_domain, :target_domain => target_domain
+    # Emit this back to the app.  This is important because it will allow
+    # Zillabyte to parallelize the operation
+    emit :source_domain => source_domain, :target_domain => target_domain
       
-    end
-    
   end
+    
 end
 
 
