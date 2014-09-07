637742c0df9730033d9a88255afffbcce99fdb58
qiongyu <xiaoyingxu@cmu.edu>
2014-09-07 00:22:32 -0700

    fix up job_openings app

--- job_openings/app.rb
+++ job_openings/app.rb
@@ -12,35 +12,30 @@ require 'equivalent-xml'
 require 'rdf/rdfa'
 require 'sparql'
 
-# We reuse two values several times over the course of this flow: the base address of the RDF schema and
-# the list of attributes that define our final schema.  So we define them here.
+# We reuse two values several times over the course of this app: the base address of the RDF schema and
+# the list of attributes that define our final schema. So we define them here.
 SCHEMA = RDF::Vocabulary.new "http://schema.org/"
 
 SCHEMA_ATTRIBUTES = ["address_locality", "hiring_organization", "title", "description", "name"]
 
-flow = Zillabyte.new "zillabyte_indeed"
+app = Zillabyte.app "zillabyte_indeed"
 
 # We start by outputting the URLs from which we wish to extract information.
 # This has been simplified considerably to only deal one page and one search term.
-flow.spout do |node|
-  node.emits [["feed", ["url"]]]
-
-  node.next_batch do |controller|
-    controller.emit "feed", {
-      "url" => "http://www.indeed.com/jobs/?q=web+crawling"
-    }
+stream = app.source do
+  next_tuple do
+    emit "url" => "http://www.indeed.com/jobs/?q=web+crawling"
   end
 end
 
 # Next, we process each URL by interpreting it as an RDF graph.
-flow.each do |node|
-  node.name "each_job_posting"
-  node.emits [["indeed_job_posting", SCHEMA_ATTRIBUTES]]
+stream = stream.each do |tuple|
+  name "each_job_posting"
 
-  node.execute do |controller, tup|
-    # We wrap the entire flow in a begin-rescue block, so that for unexpected exceptions we do not break our flow
-    # completely.  Note that one should *NOT* catch 'Exception', ever--this breaks signal handling and prevents
-    # clean shutdown of a flow.
+  execute do |tuple|
+    # We wrap the entire execute in a begin-rescue block, so that for unexpected exceptions we do not break our app
+    # completely. Note that one should *NOT* catch 'Exception', ever -- this breaks signal handling and prevents
+    # clean shutdown of an app.
     begin
       # Using the third party library, we first load the graph...
       url = tup["url"]
@@ -74,24 +69,23 @@ flow.each do |node|
         job_postal_address_query
       ).execute rdf_graph
 
-      # To close out the each node, we iterate over the solution set and emit a tuple for each record.
+      # To close out the execute, we iterate over the solution set and emit a tuple for each record.
       solution_set.each do |solution|
-        controller.emit "indeed_job_posting", solution.bindings.select { |attribute, *| SCHEMA_ATTRIBUTES.include? attribute.to_s }
+        emit solution.bindings.select { |attribute, *| SCHEMA_ATTRIBUTES.include? attribute.to_s }
       end
     rescue => e
-      # Print to standard error--this is more semantically correct and will show up during execution of `zillabyte flows:test`.
+      # Print to standard error--this is more semantically correct and will show up during execution of `zillabyte apps:test`.
       STDERR.puts "Error:\n" + e.message + "\n" + e.backtrace.join("\n")
     end
   end
 end
 
 # The last step, as always, is to sink our newly extracted information to the database for later analysis.
-flow.sink do |node|
-  node.name "indeed_job_posting"
-
-  node.column "address_locality", :string
-  node.column "hiring_organization", :string
-  node.column "title", :string
-  node.column "description", :string
-  node.column "name", :string
+stream.sink do
+  name "indeed_job_posting"
+  column "address_locality", :string
+  column "hiring_organization", :string
+  column "title", :string
+  column "description", :string
+  column "name", :string
 end
